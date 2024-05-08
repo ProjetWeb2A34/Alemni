9c90a72d6912570770dc5e78b88c2fb1aa9f2863
safa hajji <122635442+safaa-hj@users.noreply.github.com>
2024-05-08 14:42:17 +0100

    mdp oublié + stat + pdf

---
+++ Metiers/view/admin/pages/FooterF.php
@@ -0,0 +1,55 @@
+
+
+        <div class="hero_area"> 
+          <!-- header section strats -->
+    <header class="header_section">
+      <div class="container">
+        <nav class="navbar navbar-expand-lg custom_nav-container ">
+          <a class="navbar-brand" href="index.html">
+            <img src="images/logo.png" alt="" />
+            <span>
+              Alemni
+            </span>
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="d-flex ml-auto flex-column flex-lg-row align-items-center">
+              <ul class="navbar-nav  ">
+                <li class="nav-item active">
+                  <a class="nav-link" href="index.html">
+                    Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="about.html"> About </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="program.html"> Programs </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="contact.html"> Contact us</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="../Backend/pages/sign-in.php"> sign-in</a>
+                </li>
+                <!-- dropdown -->
+								<li class="dropdown header__nav-item">
+									<a class="dropdown-toggle header__nav-link header__nav-link--more" href="#" role="button" id="dropdownMenuMore" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="icon ion-ios-more"></i></a>
+
+									<ul class="dropdown-menu header__dropdown-menu" aria-labelledby="dropdownMenuMore">
+										<li><a href="CLASSE.html">Gestion Classes </a></li>
+										<li><a href="EVALUATION.html">Gestion Evaluations </a></li>
+										<li><a href="EVENT.html">Gestion Evenements </a></li>
+										<li><a href="RECLAMATION.html">Gestion Reclamations </a></li>
+                    <li><a href="PANIER.html">Gestion Panier </a></li>
+									</ul>
+								</li>
+								<!-- end dropdown -->
+
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
